d4a9c4c78cd547108e4861e9bfdaa84d047adc28
Cg-Prodigy <egomaritmbrian@gmail.com>
2023-01-13 16:42:53 +0300

    included wards to the dom

--- Forms/jarvis.js
+++ Forms/jarvis.js
@@ -1,35 +1,59 @@
-import data from './counties.json' assert {type:'json'};
+import data from './counties_data.json' assert {type:'json'};
 
 const county=document.getElementById("county")
 const sub_county=document.getElementById("sub_county")
+const ward=document.getElementById("ward")
 county.addEventListener("focus",()=>{
-    addContent(county);
-})
+    addContent();
+});
 county.addEventListener("change",()=>{
     sub_county.innerHTML="";
-})
+    ward.innerHTML="";
+});
 sub_county.addEventListener("focus",()=>{
-    addSub(sub_county);
-})
-function addContent(input){
+    addSub();
+});
+sub_county.addEventListener("change",()=>{
+    ward.innerHTML="";
+});
+ward.addEventListener("focus",()=>{
+    addWard();
+});
+
+function addContent(){
+    county.innerHTML="";
     for (let entry in data){
         let option=document.createElement("option");
         let option_text=document.createTextNode(`${entry}`);
         option.appendChild(option_text);
         option.setAttribute('value',`${entry}`);
-        input.appendChild(option);
+        county.appendChild(option);
     }
 }
-function addSub(input){
+function addSub(){
     if (county.value ===""){
         return
     }
-    for (let entry in data[county.value]){
-        let sub=data[county.value][entry];
+    sub_county.innerHTML="";
+    for(let entry in data[county.value]){
         let option=document.createElement("option");
-        let option_text=document.createTextNode(`${sub}`);
+        let option_text=document.createTextNode(`${entry}`);
         option.appendChild(option_text);
-        option.setAttribute('value',`${sub}`);
-        input.appendChild(option);
+        option.setAttribute("value",`${entry}`);
+        sub_county.appendChild(option);
+    }
+}
+function addWard(){
+    if (sub_county.value===""){
+        return;
+    };
+    ward.innerHTML="";
+    for(let entry in data[county.value][sub_county.value]){
+        let d_ward=data[county.value][sub_county.value][entry];
+        let option=document.createElement("option");
+        let opt_text=document.createTextNode(`${d_ward}`)
+        option.setAttribute("value",`${d_ward}`)
+        option.appendChild(opt_text)
+        ward.appendChild(option)
     }
 }
